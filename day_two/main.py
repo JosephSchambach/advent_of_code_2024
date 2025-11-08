59d6a7730d33abb2bf638565bc89edb316d28533
Joseph Schambach <jrsbach1996@gmail.com>
2025-11-08 05:17:57 -0600

    day two code

--- day_two/main.py
+++ day_two/main.py
@@ -1,4 +1,43 @@
+def parser(input_str):
+    raw = input_str.split('\n')
+    lines = []
+    for row in raw:
+        line = []
+        line_vals = row.split(' ')
+        for i in line_vals:
+            line.append(int(i))
+        lines.append(line)
+    return lines
 
+def descending(line):
+    for i in range(1,len(line)):
+        diff = line[i-1] - line[i]
+        if diff > 3:
+            return 0
+        elif diff < 1:
+            return 0
+    return 1
+
+def ascending(line):
+    for i in range(1,len(line)):
+        diff = line[i] - line[i-1]
+        if diff > 3:
+            return 0
+        elif diff < 1:
+            return 0 
+    return 1
+
+def processor(line):
+    if line[0] < line[1]:
+        val = ascending(line)
+    else:
+        val = descending(line)
+    return val 
 
 with open('day_two/input.txt', 'r') as f:
     TEST_INPUT = f.read()
+lines = parser(TEST_INPUT)
+s = 0
+for line in lines:
+    s += processor(line)
+print(s)
